37d597967947488ed82bebdeb7fa9fda08c8bd67
Grzegorz Prajsner <grzpra@gmx.com>
2015-09-16 14:35:35 +0200

    u-boot-odroidxu3: check if eMMC is present
    Modify the U-Boot boot script to check if eMMC device is present on
    board. Based on result of that check find the proper SD card device
    (/dev/mmcblkX). Without this patch BSP would not boot from SD card
    on boards without eMMC connected.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@arm.com>

--- recipes-bsp/u-boot/u-boot-odroidxu3/boot-linux.cmd
+++ recipes-bsp/u-boot/u-boot-odroidxu3/boot-linux.cmd
@@ -1,4 +1,16 @@
-setenv bootargs root=/dev/mmcblk1p2 rw rootwait console=ttySAC2,115200n8 consoleblank=0
+echo "Checking if eMMC is present..."
+
+mmc dev 1
+
+if mmc rescan ; then
+        setenv bootdevice "/dev/mmcblk1p2"
+        echo "eMMC device is present, root=$bootdevice"
+else
+        setenv bootdevice "/dev/mmcblk0p2"
+        echo "eMMC device is NOT present, root=$bootdevice"
+fi
+
+setenv bootargs "root=$bootdevice rw rootwait console=ttySAC2,115200n8 consoleblank=0"
 setenv bootcmd "fatload mmc 0:1 40008000 zImage; fatload mmc 0:1 46000000 exynos5422-odroidxu3.dtb; bootz 40008000 - 46000000"
 
 boot
